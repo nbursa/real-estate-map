dd9f7bfdbd4161433d5b4834a1f28fc910009fce
Nenad Bursać <nbursa@gmail.com>
2023-10-10 16:16:10 +0200

    Remove logs

--- src/components/PropertyCarousel.vue
+++ src/components/PropertyCarousel.vue
@@ -1,7 +1,6 @@
 <template>
-    <carousel :key="`carousel-${props.property.id}`" class="mb-4" :autoplay="2000" :items-to-show="1" :items-to-scroll="1"
-        :wrap-around="true" :navigation-enabled="true" :min-swipe-distance="1" :current-slide.sync="currentSlide"
-        @change="onCarouselChange">
+    <carousel class="mb-4" :autoplay="2000" :items-to-show="1" :items-to-scroll="1" :wrap-around="true"
+        :navigation-enabled="true" :min-swipe-distance="1" :current-slide.sync="currentSlide" @change="onCarouselChange">
         <slide v-for="(pictureUrl, pictureIndex) in props.property.pictures" :key="pictureIndex">
             <img class="carousel__item" :src="pictureUrl" alt="Slide" />
         </slide>
@@ -18,18 +17,15 @@ import 'vue3-carousel/dist/carousel.css'
 import { ref, watch } from 'vue';
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 
-console.log("loading carousel")
 const props = defineProps(['property'])
 
 const currentSlide = ref<number>(0);
 
 const onCarouselChange = (newSlideIndex: number) => {
-    console.log("onCarouselChange: ", newSlideIndex);
     currentSlide.value = newSlideIndex;
 };
 
 watch(() => props.property, () => {
-    console.log("watch new slide")
     currentSlide.value = 0;
 });
 </script>
