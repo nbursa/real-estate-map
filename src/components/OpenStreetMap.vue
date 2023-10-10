2dafb649a14059d647464e1e3cd879245a8ae861
Nenad Bursać <nbursa@gmail.com>
2023-10-10 16:15:57 +0200

    Set bounds

--- src/components/OpenStreetMap.vue
+++ src/components/OpenStreetMap.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="absolute top-0 left-0 right-0 bottom-0">
-    <l-map ref="map" :zoom="store.zoom" :center="store.center" :use-global-leaflet="false">
+    <l-map ref="mapInstance" :zoom="store.zoom" :center="store.center" :use-global-leaflet="false">
       <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base"
         name="OpenStreetMap"></l-tile-layer>
       <l-marker v-for="property in store.properties" :key="property.id" :lat-lng="property.coordinates"
         v-if="store.dataLoaded" @click="openMarkerPopup(property)">
         <l-icon :icon-size="[0, 0]" :icon-anchor="[0, 0]">
-          <div class="text-white font-bold bg-black rounded inline-block w-auto px-2 py-1 whitespace-nowrap">
+          <div
+            class="text-black font-bold bg-white rounded inline-block w-auto px-2 py-1 whitespace-nowrap hover:text-white hover:bg-black">
             {{ property.price.toString() }} <span>CHF</span>
           </div>
         </l-icon>
@@ -14,7 +15,7 @@
           Click to view property details
         </l-tooltip>
         <l-popup>
-          <property-popup-content :property="property" :key="property.id" />
+          <property-popup-content :property="property" />
         </l-popup>
       </l-marker>
     </l-map>
@@ -26,27 +27,33 @@ import "leaflet/dist/leaflet.css";
 
 import { useRealEstatesStore } from '../stores/realEstatesStore';
 import { LMap, LTileLayer, LMarker, LTooltip, LPopup, LIcon } from "@vue-leaflet/vue-leaflet";
-import { ref, onMounted, watch } from 'vue';
+import { ref, onMounted, watch, nextTick } from 'vue';
 import PropertyPopupContent from './PropertyPopupContent.vue';
+import L from "leaflet";
 
 const store = useRealEstatesStore();
 
 const mapInstance = ref<typeof LMap | undefined>(undefined);
 
-const zoom = ref<number>(16);
-
 const calculateZoom = () => {
-  const bounds = store.viewBounds;
-  if (bounds && store.properties.length > 0) {
-    const mapWidth = mapInstance.value?.$el.offsetWidth || 0;
-    const mapHeight = mapInstance.value?.$el.offsetHeight || 0;
+  const markers = store.properties.map((property) => property.coordinates);
+  const map = mapInstance.value;
 
-    if (mapWidth && mapHeight) {
-      const zoomWidth = bounds.getEast() - bounds.getWest() === 0 ? 1 : mapWidth / (bounds.getEast() - bounds.getWest());
-      const zoomHeight = bounds.getNorth() - bounds.getSouth() === 0 ? 1 : mapHeight / (bounds.getNorth() - bounds.getSouth());
+  if (markers.length > 0 && map) {
+    const bounds = new L.LatLngBounds([]);
 
-      store.zoom = Math.min(zoomWidth, zoomHeight);
-    }
+    markers.forEach((coords) => {
+      bounds.extend(coords);
+    });
+
+    const newBounds = [
+      [bounds.getSouthWest().lat, bounds.getSouthWest().lng],
+      [bounds.getNorthEast().lat, bounds.getNorthEast().lng]
+    ];
+
+    nextTick(() => {
+      mapInstance.value?.leafletObject?.fitBounds(newBounds, { padding: [100, 100] });
+    })
   }
 };
 
@@ -59,14 +66,18 @@ onMounted(async () => {
     await store.fetchData();
 
     mapInstance.value?.$mapObject?.on('load', () => {
-      calculateZoom();
+      nextTick(() => {
+        calculateZoom()
+      })
     });
   }
 });
 
 watch(() => store.dataLoaded, (loaded) => {
   if (loaded) {
-    calculateZoom();
+    nextTick(() => {
+      calculateZoom()
+    })
   }
 }, { immediate: true });
 </script>
