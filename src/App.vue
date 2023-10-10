ae9bd90c45753e905a51461ed641530506069cf4
Nenad Bursać <nbursa@gmail.com>
2023-10-10 16:15:04 +0200

    Remove unused

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,6 @@ import OpenStreetMap from './components/OpenStreetMap.vue';
 </script>
 
 <template>
-  <header class="absolute top-0 right-0 m-2 text-black bg-white py-2 px-4 z-50 rounded" style="z-index: 1000;">
-    <h1 class="text-2xl text-black">Real Estates Map</h1>
-  </header>
-
   <main>
     <open-street-map></open-street-map>
   </main>
